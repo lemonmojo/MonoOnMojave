b0011e7f99b7eff56eb62a7dd49614b5ff8afd11
Felix Deimel <felix@lemonmojo.com>
2018-09-22 09:38:01 +0200

    NSApplicationLoad also works from managed, hooray!

--- XamarinMacMojaveTest2/Main.cs
+++ XamarinMacMojaveTest2/Main.cs
@@ -1,11 +1,18 @@
-﻿using AppKit;
+﻿using System.Runtime.InteropServices;
+
+using AppKit;
 
 namespace XamarinMacMojaveTest2
 {
 	static class MainClass
 	{
+		[DllImport("/System/Library/Frameworks/Cocoa.framework/Cocoa", EntryPoint = "NSApplicationLoad")]
+		private static extern void NSApplicationLoad();
+
 		static void Main(string[] args)
 		{
+			NSApplicationLoad();
+
 			NSApplication.Init();
 			NSApplication.Main(args);
 		}
