7fa62a901590932b20ccc2e61ba4aa6478348c88
Felix Deimel <felix@lemonmojo.com>
2018-09-22 09:29:26 +0200

    NSApplicationLoad to the rescue!

--- XcodeMacMojaveTest2/main.m
+++ XcodeMacMojaveTest2/main.m
@@ -2,6 +2,9 @@
 #include <mono/jit/jit.h>
 
 int main(int argc, const char * argv[]) {
+    // This call fixes the strangeness !!!
+    NSApplicationLoad();
+    
     // Comment out to get rid of the strangeness
     mono_jit_init("Test");
     
